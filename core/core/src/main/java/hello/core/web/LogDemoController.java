72c6c48228a666dceaa40dbed31e6c1a7dd9c73e
muuuumbi <dnpfkdlvm123@gmail.com>
2024-01-19 08:21:15 +0900

    feat: 강의 완료

--- core/core/src/main/java/hello/core/web/LogDemoController.java
+++ core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,15 +12,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerProvider; //MyLogger가 아닌 MyLogger를 찾을 수 있는 게 주입됨
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody //문자 바로 반환
-    public String logDemo(HttpServletRequest request){
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURI().toString();
+//        MyLogger myLogger = myLoggerProvider.getObject(); //실제 필요한 시점에 MyLogger를 받아옴.
+
+        System.out.println("myLogger = " + myLogger.getClass()); //내가 만든 게 아니라 가짜가 프로바이더처럼 동작함
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(100); //테스트용
         logDemoService.logic("testId");
         return "OK";
     }
--- core/core/src/main/java/hello/core/web/LogDemoService.java
+++ core/core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,14 +2,17 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider; //Provider로 주입
+    private final MyLogger myLogger; //프록시
 
     public void logic(String id){
+//        MyLogger myLogger = myLoggerProvider.getObject(); //Provider로 빈 가져오기
         myLogger.log("service id = " + id);
     }
 }
