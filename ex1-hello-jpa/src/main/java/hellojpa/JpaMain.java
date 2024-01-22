10e6ce10506acdfd68c0608c7687f19b4c070aae
muuuumbi <dnpfkdlvm123@gmail.com>
2024-01-22 16:12:35 +0900

    feat: section 5~

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,27 +19,28 @@ public class JpaMain {
         ex) DB에 데이터 저장, 조회 등
          */
         try {
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloA");
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-//            Member member = new Member(200L, "member200");
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.setTeamId(team.getId()); //객체지향스럽지 않음
+            member.setTeam(team);
+            em.persist(member);
 
-            //회원 조회
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
+            //영속성 컨텍스트가 아닌 DB에서 가져오는 쿼리를 보고 싶으면
+            em.flush();
+            em.clear();
 
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
+            Member findMember = em.find(Member.class, member.getId());
 
-//            em.flush();
+//            Long findTeamId = findMember.getTeamId(); //객체지향스럽지 않음
+//            Team findTeam = em.find(Team.class, findTeamId);
+            Team findTeam = findMember.getTeam();
 
-//            Member findMemebr = em.find(Member.class, 101L);
-
-//            System.out.println("findMemebr.getId() = " + findMemebr.getId());
-//            System.out.println("findMemebr.getId() = " + findMemebr.getName());
+//            Team newTeam = em.find(Team.class, 100L);//100번 팀이 있다고 가정
+//            findMember.setTeam(newTeam); //연관관계 수정
 
             tx.commit(); //commit 문제 생기면 rollback 해줘야
         }catch(Exception e){
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -8,10 +8,40 @@ import java.util.Date;
 @Table(name = "Member") //DB 테이블명 명시. 해주지 않으면 class명과 같은 테이블과 연결됨
 public class Member {
     @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name")
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member() {
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne(fetch = FetchType.LAZY) //member 입장에서 many, team 입장에서 one, 지연로딩전략
+    @JoinColumn(name = "TEAM_ID") //매핑해야 하는 컬럼명
+    private Team team; //객체지향 모델링
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
